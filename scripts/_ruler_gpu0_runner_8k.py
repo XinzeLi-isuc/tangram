80717ae008bfa497692e26ca0923ebbfa8773480
Xinze Li <lixinze@users.noreply.github.com>
2026-07-31 21:20:01 +0800

    [Script] RULER 8K runners

---
+++ scripts/_ruler_gpu0_runner_8k.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""Run RULER configs 0,1,2 sequentially on GPU0 @ 8K."""
+import subprocess, sys, os
+os.chdir("/home/lixinze/cake-serve")
+os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+PY = "/home/lixinze/miniconda3/envs/cake-serve/bin/python"
+for i in [0, 1, 2]:
+    print(f"[GPU0] Config {i} @ 8K start", flush=True)
+    subprocess.run([PY, "scripts/_ruler_5way.py", "--length", "8192", "--config_idx", str(i)], check=True)
+    print(f"[GPU0] Config {i} @ 8K done", flush=True)
+print("[GPU0] ALL DONE")
